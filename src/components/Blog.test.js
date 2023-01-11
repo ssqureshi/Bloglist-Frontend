96844aa873d13172e69d17e9bce0f7f2d88696b3
unknown <ss.qureshi97@gmail.com>
2023-01-12 00:54:32 +0500

    5.16

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen, } from '@testing-library/react'
 import Blog from './Blog'
 import userEvent from '@testing-library/user-event'
 
@@ -39,11 +39,13 @@ describe('Blog tests', () => {
   })
 
   test('clicking likes twice works', async () => {
-
+    const mockHandler = jest.fn()
     render(<Blog blog={blog} handleLike={mockHandler} user={loggedUser} />)
+
     const user = userEvent.setup()
     const viewButton = screen.getByText('view')
     await user.click(viewButton)
+
     const likeButton = screen.getByText('like')
     await user.click(likeButton)
     expect(mockHandler.mock.calls).toHaveLength(1)
